848f3d96d72a3349a16731a53243dd7728819b0e
david <davidricanto@gmail.com>
2024-05-14 11:07:25 -0500

    Añadi clase que no estaba (ARbol)

---
+++ QuadTree/src/Arbol/Arbol.java
@@ -0,0 +1,22 @@
+package Arbol;
+
+public class Arbol {
+private AbstractNode raiz;
+
+public Arbol(AbstractNode raiz) {
+	super();
+	this.raiz = raiz;
+}
+
+public AbstractNode getRaiz() {
+	return raiz;
+}
+
+public void setRaiz(AbstractNode raiz) {
+	this.raiz = raiz;
+}
+
+
+
+
+}
